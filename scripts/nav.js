40d371f266b1b1d83611c389832a081885252860
Dmcdominic <dmcdominic@gmail.com>
2018-09-22 00:32:29 -0400

    Improved nav bar, added timeline, outlines for about page and contact me page.

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,3 +1,19 @@
 $(function(){
 	$("#nav-bar").load("nav.html");
+
+	var page_pathname = window.location.pathname;
+	var pages = { "index" 	: ".index-active",
+								"about" 	: ".about-active",
+								"work" 		: ".work-active",
+								"contact" : ".contact-active"};
+
+	for (var page_name in pages) {
+		if (page_pathname.includes(page_name)) {
+			var nav_link = pages[page_name];
+			setTimeout(function() {
+				$(nav_link).addClass("active");
+			}, 50);
+		}
+	}
+
 });

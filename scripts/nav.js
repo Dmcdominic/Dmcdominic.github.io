fdd96462ad6fbc456609f6df555e9a9a35c8d41e
Dmcdominic <dmcdominic@gmail.com>
2018-11-25 21:59:05 -0500

    Added a Code Samples page

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,9 +3,10 @@ $(function(){
 
 	var page_pathname = window.location.pathname;
 	var pages = { "Portfolio" 	: ".portfolio-active",
-								"about" 	: ".about-active",
-								"Projects": ".projects-active",
-								"Contact" : ".contact-active"};
+								"about" 			: ".about-active",
+								"Projects" 		: ".projects-active",
+								"Samples" 		: ".samples-active",
+								"Contact" 		: ".contact-active"};
 
 	for (var page_name in pages) {
 		if (page_pathname.includes(page_name)) {
